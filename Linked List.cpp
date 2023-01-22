e918247a39ca875029f2f96623612b88ecd22c25
Beamdamon <Beamdamon@gmail.com>
2023-01-21 17:58:12 -0700

    Added int main

--- Linked List.cpp
+++ Linked List.cpp
@@ -1,8 +1,19 @@
 
 #include <iostream>
+#include "LinkedList.h"
+#include "node.h"
+using namespace std;
 
 int main()
 {
-   
+	cout << "Welcome to the Linked Lists\n";
+	LinkedList list;
+
+	for (int i = 0; i < 100; i++)
+	{
+		list.insert(rand() % 100);
+	}
+
+	list.display();
 }
 
