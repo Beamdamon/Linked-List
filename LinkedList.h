8e1c36ec5c8c8dafdf42c4f6b12f449df93122f5
Beamdamon <Beamdamon@gmail.com>
2023-01-21 17:34:18 -0700

    finished LinkedList class

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "node.h"
+
+class LinkedList
+{
+private:
+	int length;
+	Node* head;
+
+public:
+	LinkedList();
+	void insert(int data);
+	void display();
+	int getLength;
+
+};
